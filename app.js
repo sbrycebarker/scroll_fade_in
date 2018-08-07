b9bc79abfe21bb520bab29c444a222cdc43a6f65
sbrycebarker <sbrycesti@gmail.com>
2018-08-07 00:42:05 -0600

    scroll

---
+++ app.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  // document.getElementById("myBar").style.width = scrolled + "%";
+
+  console.log("scroll at", scrolled + '%')
+}
