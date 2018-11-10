d7dd176747375ed05b83c6333c8addd699fe6a23
sbrycebarker <sbrycesti@gmail.com>
2018-11-10 04:07:40 -0700

    js

--- app.js
+++ app.js
@@ -20,38 +20,29 @@ function scrollFunction() {
 
 
   function scrollPoint(scroll) {
+    reef.style.opacity = '0';
+    deer.style.opacity = '0';
+    ele.style.opacity = '0';
+    bear.style.opacity = '0';
+    deer.pause();
+    reef.pause();
+    ele.pause();
+    bear.pause();
     if (scroll < 260) {
       console.log('reef');
       reef.play();
-      ele.style.opacity = '0';
-      deer.style.opacity = '0';
-      bear.style.opacity = '0';
       reef.style.opacity = '1';
     } if (scroll > 260 && scroll < 510) {
       console.log('deer');
       deer.play();
-      reef.pause();
-      ele.pause();
-      bear.pause();
-      reef.style.opacity = '0';
-      ele.style.opacity = '0';
-      bear.style.opacity = '0'
       deer.style.opacity = '1';
     } if (scroll > 510 && scroll < 950){
       console.log('ele');
       ele.play();
-      deer.pause();
-      reef.pause();
-      reef.style.opacity = '0';
-      deer.style.opacity = '0'
-      bear.style.opacity= '0';
       ele.style.opacity = '1';
     } if (scroll > 950) {
       console.log('marmots');
       bear.play();
-      reef.style.opacity = '0';
-      deer.style.opacity = '0';
-      ele.style.opacity = '0';
       bear.style.opacity = '1';
     }
 
