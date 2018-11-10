c27400784ffa12060c162f39d532ba188f971224
sbrycebarker <sbrycesti@gmail.com>
2018-11-10 03:03:45 -0700

    mobile

--- app.js
+++ app.js
@@ -22,24 +22,33 @@ function scrollFunction() {
   function scrollPoint(scroll) {
     if (scroll < 260) {
       console.log('reef');
+      reef.play();
       ele.style.opacity = '0';
       deer.style.opacity = '0';
       bear.style.opacity = '0';
       reef.style.opacity = '1';
     } if (scroll > 260 && scroll < 510) {
       console.log('deer');
+      deer.play();
+      reef.pause();
+      ele.pause();
+      bear.pause();
       reef.style.opacity = '0';
       ele.style.opacity = '0';
       bear.style.opacity = '0'
       deer.style.opacity = '1';
     } if (scroll > 510 && scroll < 950){
       console.log('ele');
+      ele.play();
+      deer.pause();
+      reef.pause();
       reef.style.opacity = '0';
       deer.style.opacity = '0'
       bear.style.opacity= '0';
       ele.style.opacity = '1';
     } if (scroll > 950) {
       console.log('marmots');
+      bear.play();
       reef.style.opacity = '0';
       deer.style.opacity = '0';
       ele.style.opacity = '0';
